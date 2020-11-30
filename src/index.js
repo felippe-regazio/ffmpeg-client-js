3fed5142059d9887423b401079c9cf0e4ec42efb
Felippe <felippe.moraes@zoho.com>
2020-11-30 10:48:07 -0300

    added minWidth option | added real wasm support checking

--- src/index.js
+++ src/index.js
@@ -1,61 +1,87 @@
 class FFMPEGClient {
   constructor (options) {
     this.QUEUE = [];
+    this.BUSY = false;
     this.READY = false;
+
     this.OPTIONS = options;
     this.WORKER = undefined;
+    this.MINWIDTH = options.minWidth;
     this.SUPPORTED = this._supported();
-
+    
     if (window.__FFMPEGCLIENT) {
       return window.__FFMPEGCLIENT;
     } else {
       this._init();
     }
   }
-
-  _supported() {
-    return window && window.Worker;
-  }    
-
+  
   _init () {
     if (this.SUPPORTED) {
       window.__FFMPEGCLIENT = this;
-
+      
       this._registerCallbacks(this.OPTIONS.on);
       this._registerWorker(this.OPTIONS.worker);
     } else {    
-      this.OPTIONS.on.notSupported({
-        message: 'Your environment doesn\'t support ffmpeg-worker-client or you\'re not in a Browser.'
-      });
+      this.OPTIONS.on.notSupported(this._notSupportedWarn());
     }
   }  
 
+  _supported() {
+    const canIUseWorker = window && !!window.Worker;
+    const breakpointAllowed = !this.MINWIDTH || window.innerWidth >= this.MINWIDTH;
+
+    const canIUseWasm = (() => {
+      try {
+        if (typeof WebAssembly === 'object' && typeof WebAssembly.instantiate === 'function') {
+          const minCode = Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00);
+          const wasmModule = new WebAssembly.Module(minCode);
+
+          if (wasmModule instanceof WebAssembly.Module) {
+            return new WebAssembly.Instance(wasmModule) instanceof WebAssembly.Instance;
+          }
+        }
+      } catch (e) {}
+      
+      return false;
+    })();  
+    
+    return canIUseWorker && canIUseWasm && breakpointAllowed;
+  }    
+
+  _notSupportedWarn () {
+    return {
+      notSupported: true,
+      message: 'Your environment doesn\'t supports ffmpeg-worker-client or you\'re not in a Browser.'
+    };
+  }  
+  
   _registerCallbacks (context) {
     const noop = () => {};
-
+    
     const callbacks = [
       'loading',
       'ready',
-      'busy',
       'done',
+      'busy',
       'error',
       'message',
       'notSupported'
     ];
-
+    
     callbacks.forEach(cb => {
       if (!context[cb]) {
         context[cb] = noop;
       }
     });
   }
-
+  
   _registerWorker (url) {
     this.WORKER = new Worker(url);
     
     this._listenWorker(false);
   }
-
+  
   _listenWorker (callbacks) {
     this.WORKER.onmessage = message => {
       const type = message.data.type;
@@ -63,28 +89,28 @@ class FFMPEGClient {
       
       this.OPTIONS.on.message(data);
       this.OPTIONS.on[type](data);
-
+      
       if (callbacks && callbacks[type]) {
         callbacks[type](data); 
       }
-
+      
       this._onWorkerMessage (message);
     }
   }
-
+  
   _onWorkerMessage (message) {
     const type = message.data.type;
-
+    
     switch (type) {
       case 'ready':
         this.READY = true;
         this._processQueue();
         break;
-
+      
       case 'busy':
         this.BUSY = true;
         break;
-
+      
       case 'done':
       case 'error':
         this.BUSY = false;
@@ -92,18 +118,24 @@ class FFMPEGClient {
         break;
     }
   }
-
+  
   _processQueue () {
     if (this.QUEUE.length) {
       const task = this.QUEUE.shift();
-
+      
       this.ffmpeg(task);
     }
   }
-
+  
   // ------------------------------------------------
-
+  
   ffmpeg (task) {
+    if (!this.SUPPORTED) {
+      task.on.error && task.on.error(this._notSupportedWarn());
+
+      return false;
+    }
+
     if (!this.READY || this.BUSY) {
       this.QUEUE.push(task);
     } else {
@@ -111,12 +143,24 @@ class FFMPEGClient {
         files: task.files,
         args: task.args
       });
-
+      
       return this._listenWorker(task.on);
     }
   }
-
+  
   run (task) {
     return this.ffmpeg(task);
+  }
+
+  isBusy () {
+    return this.BUSY;
+  }
+
+  isReady () {
+    return this.READY;
+  }
+
+  supported () {
+    return this.SUPPORTED;
   }
 }
